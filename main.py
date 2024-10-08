a1dc7b9095d8a74d81e0f899438e7819d80b3c72
atrepalin <atrepalin19@gmail.com>
2024-10-08 11:45:42 +0300

    Add command line arguments parser

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 # Программа: Робот в лабиринте
 # Описание: Данная программа реализует различные алгоритмы поиска пути для навигации робота по лабиринту.
 # Робот может использовать такие алгоритмы, как метод ветвей и границ, поиск в глубину (с и без эвристики),
-# поиск в ширину и алгоритм равных цен для нахождения оптимального пути.
-# Версия: 04.10.2024
+# поиск в ширину, двунаправленный поиск и алгоритм равных цен для нахождения оптимального пути.
+# Версия: 08.10.2024
 
+from functools import partial
+import getopt
+import sys
 from time import sleep
 from maze import load, State, make_move
 import numpy as np
 
-from solvers import bfs, dfs, dfs_with_cmp, ucs, bnb
+from solvers import bfs, dfs, dfs_with_cmp, ucs, bnb, bidirectional_search
 
 
 def print_header():
@@ -22,36 +25,69 @@ def print_header():
     print(
         "# The robot can use algorithms such as Branch and Bound, Depth-First Search (with and without heuristic), "
     )
-    print("# Breadth-first search, and Uniform Cost Search to find the optimal path.")
-    print("# Version: 04.10.2024")
+    print("# Breadth-first search, Bidirectional search and Uniform Cost Search to find the optimal path.")
+    print("# Version: 08.10.2024")
     print("##############################################")
 
 
-print_header()
+def main(argv):
+    print_header()
 
-file = input("Enter maze file name: ")
+    opts = dict(getopt.getopt(argv, "f:s:")[0])
 
-maze = load(file)
+    if "-f" in opts:
+        file = opts["-f"]
+    else:
+        file = input("Enter maze file name: ")
 
-state = State(maze, np.array([0, 0]), maze.shape - np.asarray([1, 1]))
+    if not file.endswith(".npy"):
+        print("Invalid file. Exiting...")
+        exit()
 
-solvers = {"bfs": bfs, "dfs": dfs, "dfs_with_cmp": dfs_with_cmp, "ucs": ucs, "bnb": bnb}
+    print("Loading maze from " + file)
+    maze = load(file)
 
-solver = input("Enter solver (bfs, dfs, dfs_with_cmp, ucs, bnb): ")
+    state = State(maze, np.array([0, 0]), maze.shape - np.asarray([1, 1]))
 
-if solver not in solvers:
-    print("Invalid solver. Exiting...")
-    exit()
+    goal_state = State(
+        maze, maze.shape - np.asarray([1, 1]), maze.shape - np.asarray([1, 1])
+    )
 
-actions = solvers[solver](state)
+    solvers = {
+        "bfs": bfs,
+        "dfs": dfs,
+        "dfs_with_cmp": dfs_with_cmp,
+        "ucs": ucs,
+        "bnb": bnb,
+        "bidirectional_search": partial(bidirectional_search, goal_state=goal_state),
+    }
 
-if actions is None:
-    print("No solution found. Exiting...")
-    exit()
+    if "-s" in opts:
+        solver = opts["-s"]
+    else:
+        solver = input(
+            "Enter solver (bfs, dfs, dfs_with_cmp, ucs, bnb, bidirectional_search): "
+        )
 
-for action in actions:
-    state = make_move(state, action)
-    state.draw()
-    sleep(0.1)
+    if solver not in solvers:
+        print("Invalid solver. Exiting...")
+        exit()
 
-input()
+    print("Solver: " + solver)
+
+    actions = solvers[solver](state)
+
+    if actions is None:
+        print("No solution found. Exiting...")
+        exit()
+
+    for action in actions:
+        state = make_move(state, action)
+        state.draw()
+        sleep(0.1)
+
+    input()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- solvers/__init__.py
+++ solvers/__init__.py
@@ -3,3 +3,4 @@ from .dfs import dfs
 from .dfs_with_cmp import dfs_with_cmp
 from .ucs import ucs
 from .bnb import bnb
+from .bidirectional_search import bidirectional_search
